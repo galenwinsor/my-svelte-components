cfaac20e01d46b756f3e216320489dc5fbdf035e
galenwinsor <galen_winsor@alumni.brown.edu>
2023-04-29 19:05:05 -0400

    feat: tabs component init

---
+++ src/lib/Tabs.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Tabs from './Tabs.svelte';
+</script>
+
+<Meta title="Tabs" component={Tabs} />
+
+<Template>
+	<Tabs inputGroupName="my-tabs" tabNames={['A', 'B', 'C']} let:inputId let:tabName>
+		<legend slot="label">My Tabs</legend>
+		<label for={inputId}>{tabName}</label>
+		<div slot="content" let:selectedTab>
+			{selectedTab}
+		</div>
+	</Tabs>
+</Template>
+
+<Story name="Example" args={{}} />
--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -10,7 +10,36 @@
 
 	let selectedTab = tabNames.length > 0 ? tabNames[0] : '';
 
-	const id = `tabs-${name}-${componentIndex}`;
+	const id = `tabs-${inputGroupName}-${componentIndex}`;
 
 	onMount(() => componentIndex++);
 </script>
+
+<!--@component
+Organize several views into tabbed sections.
+
+### Props
+- `inputGroupName': A name for the group of radio buttons that implement the tab
+functionality. Used for accessibility.
+- `tabNames`: A list of tab names.
+
+### Slots
+- `label`: If you want to include a header for the component. For the most 
+accessible implementation, use a <legend> element.
+- `content`: Display the different views here, using an if-block to handle the
+different states.
+
+### Slot Props
+- `inputId`: The ID of the input element responsible for each tab. Use to create
+a `<label>` element for each tab. The actual inputs are hidden.
+- `tabName`: The name of the tab.
+-->
+<fieldset>
+	<slot name="label" />
+	{#each tabNames as tabName, index}
+		{@const inputId = id + String(index)}
+		<input type="radio" name={inputGroupName} bind:group={selectedTab} value={tabName} />
+		<slot {inputId} {tabName} />
+	{/each}
+</fieldset>
+<slot name="content" {selectedTab} />
