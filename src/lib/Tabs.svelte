efb1d108abf033ad7de5a6eb6958c152578ece57
galenwinsor <galen_winsor@alumni.brown.edu>
2023-04-29 18:37:47 -0400

    feat: initialization

---
+++ src/lib/Tabs.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	let componentIndex = 0;
+</script>
+
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let inputGroupName: string;
+	export let tabNames: string[];
+
+	let selectedTab = tabNames.length > 0 ? tabNames[0] : '';
+
+	const id = `tabs-${name}-${componentIndex}`;
+
+	onMount(() => componentIndex++);
+</script>
